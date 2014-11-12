9e875f98765df4a3c5c0b954e2bf15342145c329
Topher <topherreynoso@gmail.com>
2014-11-12 07:20:21 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start
+
 require 'rspec'
 require 'rack/test'
 require 'webmock/rspec'
@@ -9,6 +16,8 @@ require 'oauth2'
 require 'omniauth'
 require 'omniauth-oauth2'
 require 'omniauth/plangrade'
+
+WebMock.disable_net_connect!(:allow => 'coveralls.io')
 
 RSpec.configure do |config|
   config.include WebMock::API
